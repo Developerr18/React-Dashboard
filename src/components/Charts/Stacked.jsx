665689abdc1e518e1ad45b06b2bafeea69516285
Developerr18 <dineshnikum1@gmail.com>
2025-07-26 18:28:58 +0530

    Made ecommerce page

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesDirective,
+  SeriesCollectionDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
 
-const Stacked = () => {
-    return <div>Stacked</div>;
+const Stacked = ({ width, height }) => {
+  return (
+    <ChartComponent
+      width={width}
+      height={height}
+      id="stacked-chart"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: "white" }}
+    >
+      <Inject services={[StackingColumnSeries, Legend, Category, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective
+            key={index}
+            {...item}
+            animation={{ enable: false }}
+          />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
