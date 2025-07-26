057c72f311dd216b0b189d641d32577d059b27bb
Developerr18 <dineshnikum1@gmail.com>
2025-07-27 02:32:38 +0530

    dark mode custom

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -20,7 +20,7 @@ const Area = () => {
   const { currentMode } = useStateContext();
 
   return (
-    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white rounded-3xl">
       <Header category="Area" title="Inflation Rate in percentage" />
       <div className="w-full">
         <ChartComponent
